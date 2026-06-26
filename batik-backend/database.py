e36f6a37070d1a5316c77dee45ab0ea7f5589a4f
_umannn <134380467+luthfan1234@users.noreply.github.com>
2026-06-26 15:06:41 +0700

    feat: initialize SQLAlchemy database engine and session configuration

--- batik-backend/database.py
+++ batik-backend/database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_USER = "postgres"
-DB_PASSWORD = "lutfan99"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "batik"
+import os
+
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "lutfan99")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "batik")
 
 engine = create_engine(
     f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
